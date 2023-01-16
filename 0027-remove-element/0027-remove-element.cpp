e2137b10a04c9bf41d334e74a90a5df0e5399b69
Tareq Ali <95861649+TareqAli-CS@users.noreply.github.com>
2023-01-16 20:50:22 +0200

    Time: 3 ms (64.95%), Space: 8.9 MB (42.05%) - LeetHub

--- 0027-remove-element/0027-remove-element.cpp
+++ 0027-remove-element/0027-remove-element.cpp
@@ -1,17 +1,18 @@
 class Solution {
 public:
     int removeElement(vector<int>& nums, int val) {
-        
-    int counter = 0 ;
-    for(int i = 0 ; i < nums.size() ; i++)
-    {
-        if(nums[i] == val)
+        int counter = nums.size();
+        for(int i = 0 ; i < nums.size() ; i++)
         {
-            nums[i] = INT_MAX;
-            counter++;
+            if(nums[i] == val)
+            {
+                counter--;
+                nums[i] = INT_MAX;
+            }
         }
-    }
-    sort(nums.begin() , nums.end());
-    return nums.size() - counter ;    
+        
+        sort(nums.begin(),nums.end());
+        
+        return counter;
     }
 };
