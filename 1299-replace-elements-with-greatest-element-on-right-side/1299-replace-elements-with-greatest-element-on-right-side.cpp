628e08a4560734d722d53659d1d62927374a99d3
Tareq Ali <95861649+TareqAli-CS@users.noreply.github.com>
2023-01-19 23:29:27 +0200

    Time: 2808 ms (5.08%), Space: 68.6 MB (75.98%) - LeetHub

--- 1299-replace-elements-with-greatest-element-on-right-side/1299-replace-elements-with-greatest-element-on-right-side.cpp
+++ 1299-replace-elements-with-greatest-element-on-right-side/1299-replace-elements-with-greatest-element-on-right-side.cpp
@@ -2,24 +2,35 @@ class Solution {
 public:
     vector<int> replaceElements(vector<int>& v) {
         
-        if(v.size() == 1)
+       if(v.size()==1)
+       {
+           v[0]=-1;
+           return v;
+           
+       }
+        
+        if(v.size()==2)
         {
-            v[0] = -1;
+            v[0]=v[1];
+            v[1]=-1;
             return v;
-        }else if(v.size() == 2)
-        {
-            v[0] = v[1];
-            v[1] = -1;
-            return v;
-        }
             
-        for(int i = 0 ; i < v.size() ; i++)
-        {
-            v[i] = *max_element(v.begin() + (i+1) , v.end());
         }
-        v[v.size()-1] = -1;
+
+    for(int i=0;i<v.size();i++)
+    {
+         
+
+         v[i]=*max_element(v.begin() + (i+1), v.end());
+        
+   
+
+    }
+
+
+
+v[v.size()-1]=-1;
         
         return v;
-        
     }
 };
