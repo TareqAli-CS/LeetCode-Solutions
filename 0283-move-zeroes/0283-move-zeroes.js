f468389299361e28e0e858a2b8005c4e997131b7
Tareq Ali <95861649+TareqAli-CS@users.noreply.github.com>
2024-01-06 19:31:42 +0200

    Time: 77 ms (59.70%), Space: 46.3 MB (81.78%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var moveZeroes = function(nums) {
+    let zeros = 0;
+    for(let i = 0 ;i < nums.length ;i++){
+        if(nums[i] == 0){
+            zeros++;
+            nums.splice(i,1);
+            i--;
+        }
+    }    
+    while(zeros){
+        zeros--;
+        nums.push(0);
+    }
+    return nums;
+};
