829066486e233515743875f648e202200c217673
Tareq Ali <95861649+TareqAli-CS@users.noreply.github.com>
2024-02-29 10:12:53 +0200

    Time: 87 ms (12.35%), Space: 52.5 MB (7.65%) - LeetHub

---
+++ 0071-simplify-path/0071-simplify-path.js
@@ -0,0 +1,32 @@
+/**
+ * @param {string} path
+ * @return {string}
+ */
+var simplifyPath = function(path) {
+    let stack = [];
+    for(let x of path.split('/'))
+        stack.push(x);
+    let ans = ["/"];
+    for(let x of stack){
+        if(x != "" && x != "."){
+            if(x == ".."){
+                ans.pop();
+                ans.pop();
+            }
+            else{
+                ans.push(x);
+                ans.push("/");
+            }
+        }
+    }
+    console.log(ans)
+    if(ans.length > 1)
+        ans.pop();
+    else if(ans.length == 0)
+        return "/"
+    
+    if(ans[0] != "/")
+        return "/"+ans.join("")
+    return ans.join("");
+        
+};
