b5a90f62d33c2fb147d677fdc9017d24ee420c37
Tareq Ali <95861649+TareqAli-CS@users.noreply.github.com>
2024-02-29 10:23:25 +0200

    Time: 62 ms (41.67%), Space: 51.4 MB (18.07%) - LeetHub

--- 0071-simplify-path/0071-simplify-path.js
+++ 0071-simplify-path/0071-simplify-path.js
@@ -3,29 +3,18 @@
  * @return {string}
  */
 var simplifyPath = function(path) {
-    let stack = [];
-    for(let x of path.split('/'))
-        stack.push(x);
-    let ans = ["/"];
-    for(let x of stack){
-        if(x != "" && x != "."){
-            if(x == ".."){
-                ans.pop();
-                ans.pop();
+    const stack = [];
+    const directories = path.split("/");
+    for (const dir of directories) {
+        if (dir === "." || !dir) {
+            continue;
+        } else if (dir === "..") {
+            if (stack.length > 0) {
+                stack.pop();
             }
-            else{
-                ans.push(x);
-                ans.push("/");
-            }
+        } else {
+            stack.push(dir);
         }
     }
-    if(ans.length > 1)
-        ans.pop();
-    else if(ans.length == 0)
-        return "/"
-    
-    if(ans[0] != "/")
-        return "/"+ans.join("")
-    return ans.join("");
-        
+    return "/" + stack.join("/");
 };
